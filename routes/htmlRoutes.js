069aa228b2e0688ac97679b20c8cde4d7e8b4cc2
GuyYaffeErmoza <guyyaffear@gmail.com>
2020-02-24 19:50:49 -0500

    fainlestarcure

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,16 +1,21 @@
-var db = require("../models");
-
+var path = require("path");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.sendFile(path.join(__dirname + "/../public/htmls/welcome.html"));
   });
 
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/htmls/login_page.html"));
+  });
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/htmls/index.html"));
+  });
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/htmls/explore.html"));
+  });
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(
