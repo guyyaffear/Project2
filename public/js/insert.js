59cdc25b29cc0322ba3af710b1dbd86f5d082484
annammattis <54290779+annammattis@users.noreply.github.com>
2020-02-26 19:59:20 -0500

    jquery updates

--- public/js/insert.js
+++ public/js/insert.js
@@ -1,7 +1,26 @@
-function createNewRow(post) {
-    var NewBusynessCard =$(".form-group");
+// function createNewRow(post) {
+//     var NewBusynessCard =$(".form-group");
     
-    console.log(NewBusynessCard);
-};
+//     console.log(NewBusynessCard);
+// };
 
-console.log(NewBusynessCard);
+// console.log(NewBusynessCard);
+
+
+
+var InsertData = {
+    getData: function () {
+        return $.ajax({
+            url: "/api/stops",
+            type: "GET"
+        });
+    },
+    $(document).ready(function () {
+        API.getData().then(function (data) {
+            var $data = data.map(function (stop) {
+                var $option = $("<a class=form-control id=subwayStation>").text(stop.stops);
+                return $option;
+            });
+            $("#table-data").append($data);
+        });
+    });
